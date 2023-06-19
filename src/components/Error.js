4ee1d1bf4345524b50940f14c2592ea067389446
Medetcan Diler <medetdiler1996@gmail.com>
2023-06-19 19:08:25 +0300

    Completed assignment

---
+++ src/components/Error.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Error = ({ error }) => {
+  return (
+    <div style={{color: 'red'}}>
+      {error}
+    </div>
+  )
+}
+
+export default Error
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+import Error from "./Error";
 
 function Form(props) {
   const [firstName, setFirstName] = useState("Sylvia");
   const [lastName, setLastName] = useState("Woods");
+  const [submitted, setSubmitted ] = useState([]);
+  const [errors, setErrors ] = useState([]);
 
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
@@ -12,11 +16,48 @@ function Form(props) {
     setLastName(event.target.value);
   }
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // const formData = {
+    //   firstName: firstName,
+    //   lastName: lastName
+    // }
+    if(firstName.length > 0 && lastName.length > 0 ) {
+      const formData = {
+        firstName: firstName,
+        lastName: lastName
+      }
+      setSubmitted(prev => [...prev, formData]);
+      setErrors([])
+      setFirstName('');
+      setLastName('');
+    }else {
+      setErrors(['First name have to be filled!', 'lastn name have to be filled also!'])
+      setTimeout(() => {
+        setErrors([])
+      },1000)
+    }
+    // const formData = {
+    //   firstName: e.target[0].value,
+    //   lastName: e.target[1].value
+    // }
+    // setSubmitted(prev => [...prev, formData])
+    // setFirstName('')
+    // setLastName('')
+  }
+
+  const toDisplaySubmittes = submitted.map(item => (
+    <div key={uuidv4()}>{item.firstName}{item.lastName}</div>
+  ))
+
+  console.log(submitted)
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input type="text" onChange={handleFirstNameChange} value={firstName} />
       <input type="text" onChange={handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
+      {errors.length > 0 && errors.map(error => <Error key={uuidv4()} error={error} />)}
+      {toDisplaySubmittes}
     </form>
   );
 }
